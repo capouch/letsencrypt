ee46c16407c9c368a42c80ab947c0a67d529edb7
Brian Capouch <brianc@palaver.net>
2017-07-06 16:51:36 -0400

    Keep on trying to figure this out

---
+++ simple.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//require('greenlock-express')
+require('greenlock-express').create({
+
+  server: 'https://acme-staging.api.letsencrypt.org/directory'
+
+, email: 'brianc@palaver.net'
+
+, agreeTos: true
+
+, approvedDomains: [ 'white-county-history.org', 'www.white-county-history.org' ]
+
+, app: require('express')().use('/', function (req, res) {
+    res.end('Hello, World!');
+  })
+
+, debug: true
+
+}).listen(80, 443);
+
